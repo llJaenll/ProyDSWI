f7f02f2337bc3b1c34faa16d028295df37e4de29
llJaenll <33041976+llJaenll@users.noreply.github.com>
2018-09-30 13:08:26 -0500

    principal
    principal, se puso la lista de imagenes con ng-repeat usando un json

--- controller/app.js
+++ controller/app.js
@@ -9,7 +9,40 @@ angular.module('SubastasApp', ['ngRoute'])
  
   }) 
   .controller('PrincipalController', function($scope) {
-
+    $scope.productos =[
+        {
+            producto:'casaca',
+            lugar: 'Villa Maria del Triunfo',
+            precio: 50,
+            tiempo:'2:03:40',
+            imagen:'../img/producto1.jpg'
+        },{
+            producto:'Carro',
+            lugar: 'Villa el Salvador',
+            precio: 5000,
+            tiempo:'2:03:40',
+            imagen:'../img/producto1.jpg'
+        },{
+            producto:'sombrilla',
+            lugar: 'La Molina',
+            precio: 20,
+            tiempo:'2:03:40',
+            imagen:'../img/producto1.jpg'
+        },{
+            producto:'Carpa',
+            lugar: 'San Juan de MIraflores',
+            precio: 300,
+            tiempo:'2:03:40',
+            imagen:'../img/producto1.jpg'
+        },{
+            producto:'Pantalón',
+            lugar: 'Chorillos',
+            precio: 50,
+            tiempo:'2:03:40',
+            imagen:'../img/producto1.jpg'
+        }                
+        
+    ];
  
   })
 .config(['$routeProvider', function ($routeProvider) {
